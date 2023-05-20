5ab49a0d5bbc5bf54aada4a090a5daa19f663707
Luca Pipolo <luca@somoscuatro.es>
2023-05-20 22:47:53 +0200

    style: apply code standards

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 export default function Home() {
   return (
-    <main>
-      Hi there! This is Luca Pipolo&apos;s new portfolio homepage.
-    </main>
+    <main>Hi there! This is Luca Pipolo&apos;s new portfolio homepage.</main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {},
   plugins: [],
 }
